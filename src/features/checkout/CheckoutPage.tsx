fba46a22837f41ef8bc72ed8517ad462ba7e5117
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-28 09:01:39 +0100

    fix: removed duplicates

--- src/features/checkout/CheckoutPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function CheckoutPage() {
-    return (
-        <Typography variant='h2'>
-        Only logged in users will be able to use this!
-    </Typography>
-    )
-}
